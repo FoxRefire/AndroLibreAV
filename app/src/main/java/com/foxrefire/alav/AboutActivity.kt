05bdb09fea9245e8a3752d37838ec960763b4424
FoxRefire <155989196+FoxRefire@users.noreply.github.com>
2026-03-07 03:33:28 +0900

    Improve about activity

--- app/src/main/java/com/foxrefire/alav/AboutActivity.kt
+++ app/src/main/java/com/foxrefire/alav/AboutActivity.kt
@@ -1,5 +1,7 @@
 package com.foxrefire.alav
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.foxrefire.alav.databinding.ActivityAboutBinding
@@ -17,6 +19,14 @@ class AboutActivity : AppCompatActivity() {
 
         val versionName = packageManager.getPackageInfo(packageName, 0).versionName
         binding.versionText.text = getString(R.string.about_version, versionName)
+
+        binding.linkApp.setOnClickListener { openUrl("https://github.com/FoxRefire/android-antimalware-app") }
+        binding.linkYaraX.setOnClickListener { openUrl("https://github.com/VirusTotal/yara-x") }
+        binding.linkYaraForge.setOnClickListener { openUrl("https://github.com/YARAHQ/yara-forge") }
+    }
+
+    private fun openUrl(url: String) {
+        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
     }
 
     override fun onSupportNavigateUp(): Boolean {
